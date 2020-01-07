337083e2115be113e8719113ec5c5e6abffbcfea
Abdellah lahnaoui <abdellah.lahnaoui@gmail.com>
2020-01-07 18:19:24 +0100

    Initialize the bot and welcome new users

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+import random
+
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
+    print(
+        f'{bot.user.name} is connected to the following server:\n'
+        f'{guild.name}(id: {guild.id})'
+        )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+bot.run(TOKEN)
