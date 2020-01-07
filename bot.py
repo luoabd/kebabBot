a6d43de0c1f45bd874e1b982eb4d265de718bcf8
Abdellah lahnaoui <abdellah.lahnaoui@gmail.com>
2020-01-07 18:22:32 +0100

    Added a joke and roll_dice commands

--- bot.py
+++ bot.py
@@ -19,8 +19,6 @@ async def on_ready():
         f'{bot.user.name} is connected to the following server:\n'
         f'{guild.name}(id: {guild.id})'
         )
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
 
 @bot.event
 async def on_member_join(member):
@@ -28,5 +26,21 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my Discord server!'
     )
+
+@bot.command(name='joke', help='Responds with a random joke')
+async def joke(ctx):
+    funny_jokes = [
+        'Why are eggs not very much into jokes? Because they could crack up.',
+        'You can\'t spell advertisements without semen between the tits.',
+        'If my wife made a dollar for every sexist joke I make, she\'d be $0.77 richer right now.'
+    ]
+
+    response = random.choice(funny_jokes)
+    await ctx.send(response)
+
+@bot.command(name='roll_dice', help='Simulates rolling dice')
+async def roll_dice(ctx):
+    dice = str(random.choice(range(1, 7)))
+    await ctx.send(dice)
 
 bot.run(TOKEN)
